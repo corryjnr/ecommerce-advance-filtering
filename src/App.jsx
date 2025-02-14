9d9d1f2e4797a538cd763bf0c8fa8ec8cf4b1a81
Michael Corry <corryvert@gmail.com>
2025-02-14 15:18:05 +0300

    Complete sidebar refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,74 @@
+import { useState } from 'react'
 import Nav from './Navigation/Nav'
 import Products from './Products/Products'
 import Recommended from './Recommended/Recommended'
 import Sidebar from './Sidebar/Sidebar'
+import Card from './components/Card'
 import './index.css'
 
+// Database
+import products from './db/data'
+
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  const [query, setQuery] = useState("")
+
+  // ---------- Input filter ----------
+  const handleInputChange = event => {
+    setQuery(event.target.value)
+  }
+
+  const filteredItems = products.filter(
+    product => product.title
+      .toLocaleLowerCase()
+      .indexOf(query.toLocaleLowerCase() !== -1))
+
+  // ---------- Radio filter ----------
+  const handleChange = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+  // ---------- Button filter ----------
+  const handleClick = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products
+
+    // Filtering input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected)
+    }
+
+    return filteredProducts.map(({ img, title, star, reviews, prevPrice, newPrice }) => (
+      <Card
+        key={Math.random}
+        img={img}
+        title={title}
+        star={star}
+        reviews={reviews}
+        prevPrice={prevPrice}
+        newPrice={newPrice}
+      />
+    ))
+  }
+
+  const result = filteredData(products, selectedCategory, query)
 
   return (
     <>
-      <Sidebar />
+      <Sidebar handleChange={handleChange} />
       <Nav />
       <Recommended />
       <Products />
